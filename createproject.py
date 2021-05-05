4b38dd7785090347bbe40ca0425ce8189f7ce3f6
PaxMax1 <74021835+PaxMax1@users.noreply.github.com>
2021-05-05 09:49:07 -0700

    Update createproject.py

--- createproject.py
+++ createproject.py
@@ -13,15 +13,19 @@ root.title("Amazon Guessing Game")
 # Define Frames
 gameFrame = tk.Frame(root)
 imageFrame1 = tk.Frame(root)
+imageFrame2 = tk.Frame(root)
 price1Frame = tk.Frame(root)
 price2Frame = tk.Frame(root)
-frameList = [gameFrame, imageFrame1, price1Frame, price2Frame]
+frameList = [gameFrame, imageFrame1, imageFrame2, price1Frame, price2Frame]
 
 # Configure all Frames
 for frame in frameList:
 	frame.grid(row=0, column=0, sticky='news')
 	frame.configure(bg='white')
-
+root.wm_geometry("1000x500")
+gameFrame.winfo_height=500
+gameFrame.winfo_width=1000
+root.configure(bg="white")
 # Product Descriptions
 productdescription1 = "hi"
 productdescription2 = "hi"
@@ -63,7 +67,7 @@ product2buttonimageph=ImageTk.PhotoImage(product2buttonimage)
 # Labels
 titleLabel = tk.Label(gameFrame, text="$ Which Costs More? $", font=titleFont, fg='green', bg='light green')
 titleLabel.grid(row=1, column=1, columnspan=5)
-instructionLabel = tk.Label(gameFrame, text="Click on the product that you think costs more!", bg='white', font=helpfont )
+instructionLabel = tk.Label(gameFrame, text="Click on the product that you think costs more!", bg='white', font=helpfont)
 instructionLabel.grid(row=2, column=1, columnspan=5)
 price1Label = tk.Label(gameFrame, text= price1, font=helpfont, bg='white', fg='gold')
 price1Label.grid(row=4, column=1)
@@ -71,7 +75,7 @@ price2Label = tk.Label(gameFrame, text= price2, font=helpfont, bg='white', fg='g
 price2Label.grid(row=4, column=5)
 
 # Buttons
-image2Button = tk.Button(gameFrame, command=dummy, font=buttonFont, text= productdescription2)
+image2Button = tk.Button(imageFrame2, command=dummy, font=buttonFont, text= productdescription2)
 image2Button.grid(row=5, column=5, padx=20, pady=20 )
 image1Button = tk.Button(imageFrame1, command=dummy, font=buttonFont, text= productdescription1)
 image1Button.grid(row=7, column=2,)
@@ -79,6 +83,8 @@ restartButton = tk.Button(gameFrame, command=dummy, text="Restart", font = butto
 restartButton.grid(row=5, column=3)
 moveToimage1Button = tk.Button(gameFrame, command=dummy, font=buttonFont, text=productdescription1, width=20)
 moveToimage1Button.grid(row=5, column=1)
+moveToimage2Button = tk.Button(gameFrame, command=dummy, font=buttonFont, text=productdescription1, width=20)
+moveToimage2Button.grid(row=5, column=5)
 
 # Window Launch
 raiseFrame(gameFrame)
